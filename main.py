f80c9d37828c1bfac9db3c9d5071b5d750cadfcb
ansery <anseryacine155@gmail.com>
2024-01-02 15:40:13 +0100

    update

--- main.py
+++ main.py
@@ -4,41 +4,50 @@ from pyRDDLGym import ExampleManager
 from pyRDDLGym.Core.Policies.Agents import RandomAgent, BaseAgent
 from pyRDDLGym.Visualizer.ChartViz import ChartVisualizer
 
+from pyRDDLGym.Visualizer.TextViz import TextVisualizer
+from pyRDDLGym.Visualizer.MovieGenerator import MovieGenerator
+from pprint import pprint
+import numpy as np
 
 
 
-
-class DirectAgent(BaseAgent):
-    def __init__(self, action_space, seed=None):
+class QLearningAgent:
+    def __init__(self, action_space,state_space_size, learning_rate=0.1, discount_factor=0.9, exploration_rate=0.1):
         self.action_space = action_space
-        self.rng = random.Random(seed)
-        if seed is not None:
-            self.action_space.seed(seed)
+        self.state_space_size  = state_space_size
+        self.lr = learning_rate
+        self.gamma = discount_factor
+        self.epsilon = exploration_rate
+        self.q_table = np.zeros((state_space_size, len(action_space)))
 
-    def action_horizon(self, state):
-        hackable = []
-        for key, value in state.items():
-            if key.startswith("hackable") and value:
-                action_key = key.replace("hackable", "")
-                hackable.append(action_key)               
-        return hackable
-
-    def sample_action(self, state=None):
-        possible_actions = self.action_horizon(state)
-        s = self.action_space.sample()
-        action = {}
-        if len(possible_actions) > 0:
-            selected_action = random.choice(possible_actions)
-            action[selected_action] = 1
+    def sample_action(self, state):
+        if np.random.random() < self.epsilon:
+            return self.action_space.sample()  # Explore
         else:
-            action[list(s.keys())[0]] = 0
-        return action
+            return self.action_space.sample()  # Explore
+
+    def update(self, state, action, reward, next_state):
+        old_value = self.q_table[state, action]
+        next_max = np.max(self.q_table[next_state])
+        new_value = (1 - self.lr) * old_value + self.lr * (reward + self.gamma * next_max)
+        self.q_table[state, action] = new_value
+
+
+
     
 
-env = RDDLEnv.RDDLEnv(domain='domain.rddl', instance='local_instance.rddl')
-env.set_visualizer(ChartVisualizer)
+env =  RDDLEnv.RDDLEnv(domain='domain.rddl', instance='local_instance.rddl')
 
-agent = RandomAgent(action_space=env.action_space, seed=3)
+print(env.set_visualizer(TextVisualizer))
+
+#print(len(env.action_space))
+
+#agent = QLearningAgent(action_space=env.action_space,state_space_size=5 ,learning_rate=0.05, discount_factor=0.99, exploration_rate=0.1)
+
+agent = RandomAgent(action_space=env.action_space, num_actions=env.max_allowed_actions)
+
+
+#pprint(vars(env.model))
 
 total_reward = 0
 state = env.reset()
@@ -46,7 +55,22 @@ for step in range(env.horizon):
     env.render()
     action = agent.sample_action(state)
     next_state, reward, done, info = env.step(action)
-    print(f'state = {state}, action = {action}, reward = {reward}')
+    print("\done = {}".format(done))
+    print("\nstep = {}".format(step))
+    print('reward = {}'.format(reward))
+    print('state = {}'.format(state))
+    print('action = {}'.format(action))
+    print('info = {}'.format(info))
+    print('next_state = {}'.format(next_state))
+    #print(len(env.action_space))
+
+
+    
+    #print('derived = {}'.format(env.model.derived))
+    #print('interm = {}'.format(env.model.interm))
+
+    
+    #print(f'state = {state}, action = {action}, reward = {reward}')
     total_reward += reward
     state = next_state
     if done:
